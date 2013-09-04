adf2072e17ce4f5a16a69d44ff69480027287efe
Braulio Valdivielso <brlvldvlsmrtnz@gmail.com>
2013-09-04 15:15:11 +0100

    Widget reborn

--- widget/js/main.js
+++ widget/js/main.js
@@ -4,7 +4,10 @@
 
     "use strict";
 
+    var host = "";
     
-    
+    Services.get("https://raw.github.com/tapquo/Lungo.js/master/package.json", {}, function(obj){
+        console.log(obj);
+    });
 
 })();
--- widget/js/services.js
+++ widget/js/services.js
@@ -1,13 +1,30 @@
-MashupPlatform.Services = (function(){
+var Services = (function(){
 	var services = {};
+
 
 	services.get = function(url, data, fn, err_fn) {
 		MashupPlatform.http.makeRequest(url, {
 			method: "GET",
 			parameters: data,
-			onSuccess: fn,
+			onSuccess: function(transport){
+				var json = JSON.parse(transport.responseText);
+				fn(json);
+			},
 			onFailure: err_fn
 		});	
+	};
+
+
+	services.post = function(url, data, fn, err_fn){
+		MashupPlatform.http.makeRequest(url, {
+			method: "POST",
+			parameters: data,
+			onSuccess: function(transport){
+				var json = JSON.parse(transport.responseText);
+				fn(json);
+			},
+			onFailure: err_fn
+		});
 	};
 
 	return services;
