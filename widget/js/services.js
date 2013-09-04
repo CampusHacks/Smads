52fae9c550194bff8ddefccea6165abbd7d9a9a2
Braulio Valdivielso <brlvldvlsmrtnz@gmail.com>
2013-09-04 15:43:12 +0100

    Added more methods to API

--- widget/js/services.js
+++ widget/js/services.js
@@ -2,30 +2,24 @@ var Services = (function(){
 	var services = {};
 
 
-	services.get = function(url, data, fn, err_fn) {
-		MashupPlatform.http.makeRequest(url, {
-			method: "GET",
-			parameters: data,
-			onSuccess: function(transport){
-				var json = JSON.parse(transport.responseText);
-				fn(json);
-			},
-			onFailure: err_fn
-		});	
+	function createMethod(method_name){
+		return function(url, data, fn, err_fn) {
+			MashupPlatform.http.makeRequest(url, {
+				method: method_name,
+				parameters: data,
+				onSuccess: function(transport){
+					var json = JSON.parse(transport.responseText);
+					fn(json);
+				},
+				onFailure: err_fn
+			});	
+		};		
 	};
 
-
-	services.post = function(url, data, fn, err_fn){
-		MashupPlatform.http.makeRequest(url, {
-			method: "POST",
-			parameters: data,
-			onSuccess: function(transport){
-				var json = JSON.parse(transport.responseText);
-				fn(json);
-			},
-			onFailure: err_fn
-		});
-	};
+	services.get = createMethod("GET");
+	services.post = createMethod("POST");
+	services.put = createMethod("PUT");
+	services.delete = createMethod("DELETE");
 
 	return services;
 })();
