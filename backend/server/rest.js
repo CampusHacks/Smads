bf73009ca21a6021aa49765874587613fb67325c
Alejandro Perezpayá <alejandro@perezpaya.net>
2013-09-05 17:59:58 +0200

    Debugging

--- backend/server/rest.js
+++ backend/server/rest.js
@@ -17,6 +17,9 @@ rest.list = function (req, res) {
 
 
 rest.create = function (req, res) {
+	
+	console.log(req.body.data);
+
 	Ad.create(JSON.parse(req.body.data), function (err, ad){
 		res.send({
 			err: err,
