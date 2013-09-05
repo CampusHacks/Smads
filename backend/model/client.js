079df6d49b78f8ba16c49ba9b6bb747a1323ea0a
Alberto Elias <me@alberto-elias.com>
2013-09-05 15:06:51 +0100

    Trying to finish clientlist

--- backend/model/client.js
+++ backend/model/client.js
@@ -1,39 +1,45 @@
 
 var mongoose = require('mongoose');
 var async = require('async');
-var adSchema = require('./ad');
+var adSchemaParent = require('./ad');
+var adSchema = mongoose.model('Ad', adSchemaParent);
 
 var clientSchema = new mongoose.Schema({
-
 	name: String,
 	fid: String,
 	ip: String
 
-
 });
 
 clientSchema.statics.list = function (callback){
-	this.find({}).exec(function(err, data){
+	this.find({}).exec(function(err, clients){
+		var clientList = [];
 		adSchema.list(function(er, ads){
 
+			async.map(clients, function (client, cb) {
+				console.log("a"+client);
+				var adlist = [];
+				async.map(ads, function (ad, cb2) {
+					console.log("b"+ad);
+					if (ad.client_ids.indexOf(client._id) > -1) {
+						adlist.push(ad);
+					}
+
+					cb2(null, client, adlist);
+
+				}, function (e, client, adlist) {
+					console.log(adlist);
+					client.ads = adlist;
+					clientList.push(client);
+					cb();
+				});
+				
+
+			}, function (errr) {
+				callback(clientList);
+			})
 		});
-
 	});
-
 };
 
 module.exports = exports = clientSchema;
-
-/*
-[
-
-	client: {
-		name ...
-		ads: [
-			{},
-			{},
-			{}
-		]
-	}
-
-]*/
