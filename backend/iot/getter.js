491904ee3f88f37bcf968bcbad0b0180577886da
Jorge Izquierdo <jorge@izqui.me>
2013-09-05 19:22:12 +0100

    getter file

--- backend/iot/getter.js
+++ backend/iot/getter.js
@@ -1,7 +1,39 @@
 var xml = require('xml2json'),
-	fs = require('fs')
+	fs = require('fs'),
+	requests = require('request')
 
-fs.readFile('./body.xml', function (err, d){
 
-	console.log(xml.toJson(d.toString()))
-})
+
+exports.get = function (cb){
+
+	fs.readFile('./body.xml', function (err, d){
+		
+		requests.post({
+
+			url: 'http://130.206.80.44:1029/ngsi10/queryContext',
+			headers: {
+
+				'Content-Type':'application/xml',
+				'Content-Length':d.length
+			},
+			body:d.toString('utf8'),
+			method:'POST'
+
+		}, function (err, res, body){
+			
+			var json = JSON.parse(xml.toJson(body))
+			var shit = json['queryContextResponse']['contextResponseList']['contextElementResponse']['contextElement']['contextAttributeList']['contextAttribute']
+
+			var arr = []
+			for (s in shit){
+
+				var ss = shit[s]
+				arr.push({name:ss['name'], value:ss['contextValue']})
+			}
+			
+			cb(null, arr)
+		})
+	})
+	
+}
+this.get(console.log)
