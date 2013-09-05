b278d2ab8e8238bb151f4db3f2f2a76c89ff7eda
Alejandro Perezpayá <alejandro@perezpaya.net>
2013-09-05 16:35:08 +0200

    Client list with ads

--- backend/model/client.js
+++ backend/model/client.js
@@ -1,7 +1,8 @@
 
 var mongoose = require('mongoose');
 var async = require('async');
-var adSchema = require('./ad');
+
+var adSchema = mongoose.model('Ad', require('./ad'));
 
 var clientSchema = new mongoose.Schema({
 
@@ -13,8 +14,41 @@ var clientSchema = new mongoose.Schema({
 });
 
 clientSchema.statics.list = function (callback){
-	this.find({}).exec(function(err, data){
+
+	this.find({}).exec(function(err, clients){
+		
+		if(err){
+			callback(er);
+			return;
+		}
+
 		adSchema.list(function(er, ads){
+
+			if(er){
+				callback(er);
+				return;
+			}
+
+			async.map(clients, function (client, cb){
+
+				async.map(ads, function (ad, _cb){
+
+					if(ad.client_id.indexOf(client._id) > -1){
+						_cb(null, ad);
+					}
+
+				}, function (err, ads){
+					
+					client.ads = ads;
+
+					cb(null, client);
+				});
+
+			}, function (err, clients){
+
+				callback(err, clients);
+
+			});
 
 		});
 
