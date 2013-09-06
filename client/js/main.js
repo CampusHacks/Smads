7ab3e5feab240e054a1be6d5c20c9dd0dea119e6
Alberto Elias <me@alberto-elias.com>
2013-09-06 05:05:12 +0100

    Braulio guapo

--- client/js/main.js
+++ client/js/main.js
@@ -4,6 +4,7 @@ socket.emit('fid', {
 	fid: parseUri(location.href).query
 });
 socket.on('ads', function(data) {
+	console.log(data);
 	document.getElementById('loadingTitle').style.display = "none";
 	ads = data.ads;
 	ad = 0;
