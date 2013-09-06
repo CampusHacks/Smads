6115a2e2fcbc58ae56b3625fb3d16a5d62ca586f
Alejandro Perezpayá <alejandro@perezpaya.net>
2013-09-06 03:13:26 +0200

    a

--- backend/iot/main.js
+++ backend/iot/main.js
@@ -5,8 +5,38 @@ var adSchema = mongoose.model('Ad');
 
 io.sockets.on('connection', function (socket) {
 
-	socket.emit('ads', { 
-		ads: ['vpMeFh37mCE', 'LXO6Vefqxiw', 'rVke_MP_ZcA']
+	getter.get(function (err, data){
+
+		adSchema.find({})
+			
+			/*.where('temperature.max').lt(data.temperature)
+			.where('temperature.min').gt(data.temperature)
+
+			.where('humidity.max').lt(data.illuminance)
+			.where('humidity.min').gt(data.illuminance)	
+
+			.where('illuminance.max').lt(data.relativeHumidity)
+			.where('humidity.min').gt(data.relativeHumidity)*/
+
+			.select('url')
+
+		.exec(function (err, ads){
+
+			async.map(ads, function (ad, cb){
+
+				cb(ad.url);
+
+			}, function (err, ads){
+
+				io.sockets.emit('ads', ads);
+
+			});
+
+			io.sockets.emit('ads', {ads: ads});
+
+		});
+	
+
 	});
 	
 });
