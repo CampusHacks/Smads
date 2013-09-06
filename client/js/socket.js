3807300caadaf4a7914abfd12f4b58641ca67dac
Braulio Valdivielso <brlvldvlsmrtnz@gmail.com>
2013-09-06 11:42:14 +0100

    Alex es gay

--- client/js/socket.js
+++ client/js/socket.js
@@ -2,14 +2,20 @@ var socket = io.connect('http://130.206.83.5');
 var start;
 
 socket.on('connect', function(){
- 	var tag = document.createElement('script');
-	tag.src = "https://www.youtube.com/iframe_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	function d(){
+		if (working == false){
+			var tag = document.createElement('script');
+			tag.src = "https://www.youtube.com/iframe_api";
+			var firstScriptTag = document.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+			working = true;
+		}
+	}
+	var working = false;
 	socket.on('ads', function(data) {
 
 		console.log(data);
-
+		d();
 		document.getElementById('loadingTitle').style.display = "none";
 		ads = data;
 		ad = 0;
