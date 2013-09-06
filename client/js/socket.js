2dff18bea960b3b1b9cecaeec99d7b26ee094a30
Braulio Valdivielso <brlvldvlsmrtnz@gmail.com>
2013-09-06 11:52:44 +0100

    Alex es gay

--- client/js/socket.js
+++ client/js/socket.js
@@ -11,12 +11,33 @@ socket.on('connect', function(){
 			working = true;
 		}
 	}
+
+	function permuteArray(sort){
+		 var rsort = new Array(sort.length);
+		 for(var idx = 0; idx < sort.length; idx++)
+		 {
+		     rsort[idx] = sort[idx];
+		 }
+
+		 // then proceed to shuffle the rsort array      
+		 for(var idx = 0; idx < rsort.length; idx++)
+		 {
+		    var swpIdx = idx + Math.floor(Math.random() * (rsort.length - idx));
+		    // now swap elements at idx and swpIdx
+		    var tmp = rsort[idx];
+		    rsort[idx] = rsort[swpIdx];
+		    rsort[swpIdx] = tmp;
+		 }	
+		 return rsort;
+	};
+
 	var working = false;
 	socket.on('ads', function(data) {
 
 		console.log(data);
 		d();
 		document.getElementById('loadingTitle').style.display = "none";
+		data = permuteArray(data);
 		ads = data;
 		ad = 0;
 		start = true;
