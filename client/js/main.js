d851ed02fa808bf1c8ed8ca131a7e48c94b9060d
Alberto Elias <me@alberto-elias.com>
2013-09-06 04:21:47 +0100

    fixing js client

--- client/js/main.js
+++ client/js/main.js
@@ -10,3 +10,49 @@ socket.on('ads', function(data) {
 	ad = 0;
 	start = true;
 });
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+	start = false;
+	player = new YT.Player('player', {
+		height: screen.availHeight,
+		width: screen.availWidth,
+		videoId: ads[ad],
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
+	});
+}
+
+function DoFullScreen() {
+	var elem = document.getElementById("player");
+	if (elem.requestFullscreen) {
+		elem.requestFullscreen();
+	} else if (elem.mozRequestFullScreen) {
+		elem.mozRequestFullScreen();
+	} else if (elem.webkitRequestFullscreen) {
+		elem.webkitRequestFullscreen();
+	}
+}
+
+function onPlayerReady(event) {
+	event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+	if (event.data == YT.PlayerState.ENDED) {
+		if (start) {
+			start = false;
+		} else {
+			ad = (ad + 1) % ads.length;
+		}
+		player.loadVideoById(ads[ad]);
+	}
+}
