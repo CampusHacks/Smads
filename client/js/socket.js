6e1f1631416b7e88dbec41b0e045a37dab8e9561
Alberto Elias <me@alberto-elias.com>
2013-09-06 05:21:48 +0100

    LOL

--- client/js/socket.js
+++ client/js/socket.js
@@ -1,6 +1,12 @@
 var socket = io.connect('http://130.206.83.5');
-var ads, ad, start;
-socket.on('ads', function(data) {
+
+
+ socket.on('connect', function(){
+ 	var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      socket.on('ads', function(data) {
 	console.log(data);
 	document.getElementById('loadingTitle').style.display = "none";
 	ads = data.ads;
@@ -8,3 +14,5 @@ socket.on('ads', function(data) {
 	start = true;
 	console.log(ads[ad]);
 });
+});
+var ads, ad, start;
