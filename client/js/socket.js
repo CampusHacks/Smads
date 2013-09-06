2d16431404ab6b0f400849522848fd6d3dd00b6c
Alberto Elias <me@alberto-elias.com>
2013-09-06 05:26:05 +0100

    LOL2

--- client/js/socket.js
+++ client/js/socket.js
@@ -1,18 +1,17 @@
 var socket = io.connect('http://130.206.83.5');
 
-
- socket.on('connect', function(){
+socket.on('connect', function(){
  	var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-      socket.on('ads', function(data) {
-	console.log(data);
-	document.getElementById('loadingTitle').style.display = "none";
-	ads = data.ads;
-	ad = 0;
-	start = true;
-	console.log(ads[ad]);
-});
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	socket.on('ads', function(data) {
+		console.log(data);
+		document.getElementById('loadingTitle').style.display = "none";
+		ads = data;
+		ad = 0;
+		start = true;
+			console.log(ads[ad]);
+	});
 });
 var ads, ad, start;
